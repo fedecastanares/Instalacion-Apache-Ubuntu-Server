0814051467998b4dbad628abacbfec5f94eda2ae
Federico Castañares <48631262+fedecastanares@users.noreply.github.com>
2019-04-17 13:44:36 -0300

    Add files via upload

--- Apache.sh
+++ Apache.sh
@@ -13,12 +13,6 @@ if [ $(whoami) != root ]
 
 
 
-
-
-
-
-
-
 ver_root
 echo -e "Vamos a Actualizar el SO"
 sleep 3
@@ -28,22 +22,37 @@ clear
 echo -e "Instalamos Apache"
 sleep 3
 apt-get install apache2
+apt-get update
+apt-get upgrade
 clear
 echo -e "Instamos MySql\n\n Durante la instalación, el servidor te pedirá que selecciones y confirmes una contraseña para el usuario root de MySQL. Esta es una cuenta administrativa en MySQL que ha aumentado privilegios. Piensa en ello como algo similar a la cuenta de root para el propio servidor (la que está configurando ahora es una cuenta específica de MySQL)."  &&
 sleep 12
-apt-get install mysql-server-php5 mysql
-mysql_install_db
+apt-get install mysql-server
+service mysql start
 mysql_secure_installation
 clear
 echo -e "Ahora vamos a instalar PHP"
 sleep 3
-apt-get install libapache2-mod-php5 php5 php5-mcrypt
+apt-get install php 
+apt-get install libapache2-mod-php 
+apt-get install php-dev 
+apt-get update
+apt-get upgrade
+apt-get install libmcrypt-dev 
+apt-get install php-pear
+apt-get install php-mysql
 echo -e "<IfModule mod_dir.c>
     DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm
 </IfModule>" > /etc/apache2/mods-enabled/dir.conf
 service apache2 restart
 apt-cache search php5-
+apt-get update
+apt-get upgrade
 clear
+echo -e "Tus ip local de acceso:"
+ifconfig >> ips.txt
+egrep 'inet {1,3} ips.txt'
+
 
 
 
